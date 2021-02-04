2506d07253d80e9986a924ddeeb7b490b61002f1
klynn726 <49536087+klynn726@users.noreply.github.com>
2021-02-04 15:43:01 -0700

    questions started

--- assets/script.js
+++ assets/script.js
@@ -10,9 +10,48 @@ document.body.appendChild(submitButton);            //adds the button to the bod
 
 
 var quiz = function() {
-  preventDefault();
+  preventDefault();               
 
 }
+
+var questions = [
+{
+  question: "What language uses syntax that begins with $",
+  answers: {
+    a: "DOM", b: "Javascript", c: "JQuery", d: "CSS"
+  },
+  correctAnswer: "c"
+},
+
+{
+question: "Where is the Javascript tag located on the HTML document?",
+answers: {a:"in the head", b:"at the top of the body", c:"in the header", d:"at the bottom of the body"},
+correctAnswer: "d"
+},
+
+{
+question: "What can be used to create a button using the DOM instead of HTML only?",
+answers: {a:"getElementById", b:"createElement", c:"querySelector", d:"appendChild"},
+correctAnswer: "b"
+},
+
+{
+question: "What can be used to attach a button created using the DOM to the HMTL?",
+answers: {a:"getElementById", b:"createElement", c:"querySelector", d:"appendChild"},
+correctAnswer: "d"
+},
+
+{
+  question:"",
+
+},
+
+
+
+
+]
+
+
 
 // Timer that counts down from 5
 function countdown() {
@@ -22,12 +61,12 @@ function countdown() {
     // As long as the `timeLeft` is greater than 1
     if (timeLeft > 1) {
       // Set the `textContent` of `timerEl` to show the remaining seconds
-      timerEl.textContent = timeLeft + ' seconds remaining';
+      timerEl.textContent = 'Timer: ' + timeLeft + ' seconds remaining';
       // Decrement `timeLeft` by 1
       timeLeft--;
     } else if (timeLeft === 1) {
       // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-      timerEl.textContent = timeLeft + ' second remaining';
+      timerEl.textContent = 'Timer: ' + timeLeft + ' second remaining';
       timeLeft--;
     } else {
       // Once `timeLeft` gets to 0, set `timerEl` to an empty string
@@ -41,7 +80,7 @@ function countdown() {
 }
 
 var stopQuiz = function(){
-  
+
 }
 
 
