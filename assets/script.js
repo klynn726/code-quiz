a66307a62e8ee8c0b85009fae48183e347615a79
klynn726 <49536087+klynn726@users.noreply.github.com>
2021-02-05 18:58:13 -0700

    added logic ideas

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,5 @@
 var timerEl = document.getElementById('countdown');
-
-
-var startPageEl = document.querySelector("#page-content");
+var timeLeft = 60;
 
 var submitButton = document.createElement("BUTTON");   // Create a <button> element
 submitButton.innerHTML = "Start";                   // text inside the buton
@@ -9,79 +7,11 @@ submitButton.className = "btn";                     // adds a class so CSS can t
 document.body.appendChild(submitButton);            //adds the button to the body of the html
 
 
-var quiz = function() {
-  preventDefault();               
-
-}
-
-var questions = [
-{
-  question: "What language uses syntax that begins with $",
-  answers: {a: "DOM", b: "Javascript", c: "JQuery", d: "CSS"},
-  correctAnswer: "c"
-},
-
-{
-question: "Where is the Javascript tag located on the HTML document?",
-answers: {a:"in the head", b:"at the top of the body", c:"in the header", d:"at the bottom of the body"},
-correctAnswer: "d"
-},
-
-{
-question: "What can be used to create a button using the DOM instead of HTML only?",
-answers: {a:"getElementById", b:"createElement", c:"querySelector", d:"appendChild"},
-correctAnswer: "b"
-},
-
-{
-question: "What can be used to attach a button created using the DOM to the HMTL?",
-answers: {a:"getElementById", b:"createElement", c:"querySelector", d:"appendChild"},
-correctAnswer: "d"
-},
-
-{
-question: "What can be used to select a specific HTML element by id?",
-answers: {a:"getElementById", b:"createElement", c:"setAttribute", d:"appendChild"},
-correctAnswer: "a"
-},
-
-{
-question: "What can be used to create an id for a DOM element?",
-answers: {a:"getElementById", b:"createElement", c:"setAttribute", d:"appendChild"},
-correctAnswer: "c"
-},
-
-{
-question: "What can be used to target the first element with a specific selector?",
-answers: {a:"getElementById", b:"querySelector", c:"createElement", d:"appendChild"},
-correctAnswer: "b"
-},
-
-{
-question: "What is not an example of a mouse event?",
-answers: {a:"trigger", b:"click", c:"contextmenu", d:"mouseover"},
-correctAnswer: "a"
-},
-
-{
-question: "What stops the page from reloading when inputs are entered?",
-answers: {a:"stopReload", b:"null", c:"preventDefault", d:"avoidRefresh"},
-correctAnswer: "c"
-},
-
-{
-question: "An array is contained in which of the following:",
-answers: {a:"{}", b:"()", c:"''", d:"[]"},
-correctAnswer: "d"
-},
-
-]
 
 
 
 // Timer that counts down from 5
 function countdown() {
-  var timeLeft = 60;
   // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function() {
     // As long as the `timeLeft` is greater than 1
@@ -105,11 +35,113 @@ function countdown() {
 
 }
 
+//switches between questions
+//time check
+//check for more obj in question array trigger end quiz 
+var setNextQuestion = ()=>{
+
+}
+
+//shows right/wrong
+
+var showQuestion = () =>{
+
+}
+
+//if correct + to score else minus off time
+var selectAnswer = () =>{
+
+}
+
+//high score and set to local storage
+//retrieve local storage with high scores list
+var highScore =()=>{
+
+}
+
+
+// var questionsLoop = funtion(questions){
+  // for (i = 0; i < questions.length; i++) {
+  //   document.getElementById("page-content").innerHTML = questions[0];
+    
+  // }
+// }
+
+var startQuiz = function(event) {
+  event.preventDefault();  
+  countdown();
+  
+}
+
+//triggers high score function
+//and clear timer
 var stopQuiz = function(){
 
 }
 
 
-submitButton.addEventListener('click', countdown);
+submitButton.addEventListener('click', startQuiz);
 
 
+var questions = [
+  {
+  question: "What language uses syntax that begins with $",
+  answers: {a: "DOM", b: "Javascript", c: "JQuery", d: "CSS"},
+  correctAnswer: "c"
+  },
+  
+  {
+  question: "Where is the Javascript tag located on the HTML document?",
+  answers: {a:"in the head", b:"at the top of the body", c:"in the header", d:"at the bottom of the body"},
+  correctAnswer: "d"
+  },
+  
+  {
+  question: "What can be used to create a button using the DOM instead of HTML only?",
+  answers: {a:"getElementById", b:"createElement", c:"querySelector", d:"appendChild"},
+  correctAnswer: "b"
+  },
+  
+  {
+  question: "What can be used to attach a button created using the DOM to the HMTL?",
+  answers: {a:"getElementById", b:"createElement", c:"querySelector", d:"appendChild"},
+  correctAnswer: "d"
+  },
+  
+  {
+  question: "What can be used to select a specific HTML element by id?",
+  answers: {a:"getElementById", b:"createElement", c:"setAttribute", d:"appendChild"},
+  correctAnswer: "a"
+  },
+  
+  {
+  question: "What can be used to create an id for a DOM element?",
+  answers: {a:"getElementById", b:"createElement", c:"setAttribute", d:"appendChild"},
+  correctAnswer: "c"
+  },
+  
+  {
+  question: "What can be used to target the first element with a specific selector?",
+  answers: {a:"getElementById", b:"querySelector", c:"createElement", d:"appendChild"},
+  correctAnswer: "b"
+  },
+  
+  {
+  question: "What is not an example of a mouse event?",
+  answers: {a:"trigger", b:"click", c:"contextmenu", d:"mouseover"},
+  correctAnswer: "a"
+  },
+  
+  {
+  question: "What stops the page from reloading when inputs are entered?",
+  answers: {a:"stopReload", b:"null", c:"preventDefault", d:"avoidRefresh"},
+  correctAnswer: "c"
+  },
+  
+  {
+  question: "An array is contained in which of the following:",
+  answers: {a:"{}", b:"()", c:"''", d:"[]"},
+  correctAnswer: "d"
+  },
+  
+  ]
